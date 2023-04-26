6acbb169ff4847e39d177dbec99ae161423be37a
Syam <syam@mealbrite.com>
2023-04-26 12:19:33 +0530

    factory pattern project creation

---
+++ factorypattern/src/main/java/com/syam/oodesignpatterns/factorypattern/App.java
@@ -0,0 +1,13 @@
+package com.syam.oodesignpatterns.factorypattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
